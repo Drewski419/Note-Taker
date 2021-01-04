67b323f5bf3467026cb1e5ae1e579d8bf139e2e2
Bryan Escudero <Ajefferson419@yahoo.com>
2021-01-03 20:38:49 -0800

    more changes to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,15 +27,13 @@ module.exports = function(app){
 
     // POST Method to add notes
     app.post("/api/notes", function(req, res){
-console.log(res.body)
+
         // Set unique id to entry
         if (notesData.length == 0){
             req.body.id = "0";
         } else{
 var lastindex = notesData.length - 1
 var lastindexid = notesData[lastindex].id
-
-console.log("asdf", lastindex, lastindexid, notesData[lastindex])
 var temp = JSON.parse(lastindexid) + 1
 
 
